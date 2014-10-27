aae1fe1686b302ef6eba6716ad11edf554fe8b27
Samuel Koprivnjak <samuel.koprivnjak@gmail.com>
2014-10-27 22:12:44 +0100

    Some small cosmetic changes

--- src/org/duckdns/valci/opensongbook/LoginView.java
+++ src/org/duckdns/valci/opensongbook/LoginView.java
@@ -4,42 +4,45 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.themes.ValoTheme;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class LoginView extends VerticalLayout implements View {
-	public LoginView() {
-		setSizeFull();
-		setSpacing(true);
-		Label label = new Label("Enter your information below to log in.");
-		TextField username = new TextField("Username");
-		username.setId("usernameTextBoxLoginView");
-		TextField password = new TextField("Password");
-		password.setId("passwordTextBoxLoginView");
-		addComponent(label);
-		addComponent(username);
-		addComponent(password);
-		addComponent(loginButton());
-	}
+    public LoginView() {
+        setSizeFull();
+        setSpacing(true);
+        Label labelWelcome = new Label("OpenSongbook web service");
+        labelWelcome.addStyleName(ValoTheme.LABEL_H1);
+        Label labelIntro = new Label(
+                "Welcome. Please enter your information below to log in. (or leave blank for demo purposes)");
+        TextField username = new TextField("Username");
+        username.setId("usernameTextBoxLoginView");
+        TextField password = new TextField("Password");
+        password.setId("passwordTextBoxLoginView");
+        addComponent(labelWelcome);
+        addComponent(labelIntro);
+        addComponent(username);
+        addComponent(password);
+        addComponent(loginButton());
+    }
 
-
-	private Button loginButton() {
-		Button button = new Button("Log In", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				getUI().getNavigator().navigateTo(OpensongbookUI.SONGEDITOR);
-			}
-		});
-		button.setId("loginButton");
-		return button;
-	}
-
+    private Button loginButton() {
+        Button button = new Button("Log In", new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                getUI().getNavigator().navigateTo(OpensongbookUI.SONGEDITOR);
+            }
+        });
+        button.setId("loginButton");
+        return button;
+    }
 
     @Override
     public void enter(ViewChangeEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
 }
